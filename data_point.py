31f57661da7cdad1bbd5f15f9317714cd77b6911
Tian Wang <twang95@stanford.edu>
2017-02-15 11:51:10 -0800

    make sure data_point loads the proto

--- data_point.py
+++ data_point.py
@@ -1,3 +1,5 @@
+from stanza.nlp import CoreNLP_pb2
+
 class DataPoint(object):
     def __init__(self, sample_id, q1_id, q2_id, q1_text, q2_text, q1_proto, q2_proto, is_duplicate):
         self.sample_id = sample_id
@@ -5,6 +7,8 @@ class DataPoint(object):
         self.q2_id = q2_id
         self.q1_text = q1_text
         self.q2_text = q2_text
-        self.q1_proto = q1_proto
-        self.q2_proto = q2_proto
+        self.q1_proto = CoreNLP_pb2.Document()
+        self.q1_proto.ParseFromString(q1_proto)
+        self.q2_proto = CoreNLP_pb2.Document()
+        self.q2_proto.ParseFromString(q2_proto)
         self.is_duplicate = is_duplicate
